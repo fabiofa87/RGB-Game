ed897c3913ea42082453eaff60afb340309ebec4
fabiofa87 <64399840+fabiofa87@users.noreply.github.com>
2020-08-23 16:57:12 -0700

    Add files via upload

---
+++ rgb-game.js
@@ -0,0 +1,142 @@
+let numSquares = 6;
+let colors = [];
+let pickedColor;
+let squares = document.querySelectorAll('.square');
+let colorDisplay = document.getElementById('addRgb');
+let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
+let resetButton = document.querySelector('#reset');
+let modeButtons = document.querySelectorAll('.mode');
+
+init();
+
+function init() {
+	setupModButton();
+	setUpSquares();
+	reset();
+}
+
+function setUpSquares() {
+	for (let i = 0; i < squares.length; i++) {
+		squares[i].addEventListener('click', function() {
+			let clickedColor = this.style.backgroundColor;
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = 'Correct!';
+				resetButton.textContent = 'Play Again';
+				changeColor(clickedColor);
+				h1.style.backgroundColor = clickedColor;
+			} else {
+				this.style.backgroundColor = '#232323';
+				messageDisplay.textContent = 'Try Again!';
+			}
+		});
+	}
+}
+
+function setupModButton() {
+	for (let i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener('click', function() {
+			modeButtons[0].classList.remove('selected');
+			modeButtons[1].classList.remove('selected');
+			this.classList.add('selected');
+			this.textContent === 'Easy' ? (numSquares = 3) : (numSquares = 6);
+			reset();
+			// if (this.textContent === 'Easy') {
+			// 	numSquares = 3;
+			// } else {
+			// 	numSquares = 6;
+			// }
+		});
+	}
+}
+
+resetButton.addEventListener('click', function() {
+	reset();
+	// colors = generateRandomColors(numSquares);
+	// pickedColor = pickColor();
+	// colorDisplay.textContent = pickedColor;
+	// this.textContent = 'New Colors';
+	// messageDisplay.textContent = '';
+	// for (let i = 0; i < squares.length; i++) {
+	// 	squares[i].style.backgroundColor = colors[i];
+	// }
+	// h1.style.backgroundColor = 'steelblue';
+});
+
+function reset() {
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = 'New Colors';
+	messageDisplay.textContent = '';
+	for (let i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.display = 'block';
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = 'none';
+		}
+	}
+	h1.style.backgroundColor = 'steelblue';
+}
+
+// easyBtn.addEventListener('click', function() {
+// 	hardBtn.classList.remove('selected');
+// 	easyBtn.classList.add('selected');
+// 	numSquares = 3;
+// 	colors = generateRandomColors(numSquares);
+// 	pickedColor = pickColor();
+// 	colorDisplay.textContent = pickedColor;
+// 	for (let i = 0; i < squares.length; i++) {
+// 		if (colors[i]) {
+// 			squares[i].style.backgroundColor = colors[i];
+// 		} else {
+// 			squares[i].style.display = 'none';
+// 		}
+// 	}
+// });
+// hardBtn.addEventListener('click', function() {
+// 	easyBtn.classList.remove('selected');
+// 	hardBtn.classList.add('selected');
+// 	numSquares = 6;
+// 	colors = generateRandomColors(numSquares);
+// 	pickedColor = pickColor();
+// 	colorDisplay.textContent = pickedColor;
+// 	for (let i = 0; i < squares.length; i++) {
+// 		squares[i].style.backgroundColor = colors[i];
+// 		squares[i].style.display = 'block';
+// 	}
+// });
+
+function changeColor(color) {
+	for (let i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	let random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//make an array
+	let arr = [];
+	//repeat num times
+	for (let i = 0; i < num; i++) {
+		arr.push(randomColor());
+		//get random color and push into array
+	}
+	//return array at the end
+	return arr;
+}
+
+function randomColor() {
+	//pick a "red" from 0 - 255
+	let r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255
+	let g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 - 255
+	let b = Math.floor(Math.random() * 256);
+	return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
